e37b834eb57f3c51d7b30e6081dcc9b842786c6b
Dmitry <dima.chu.00@mail.ru>
2022-03-01 23:54:40 +0300

    HW_01.03.22

---
+++ 01.03.22/Device.py
@@ -0,0 +1,21 @@
+class Device:
+    def __init__(self, type, model, data):
+        self.type = str(type)
+        self.model = str(model)
+        self.date = str(data)
+class CoffeMachine(Device):
+    def __init__(self, type, model, date, price, year, typeOfCoffe):
+        super().__init__(type, model, date)
+        self.price = int(price)
+        self.year = int(year)
+        self.type_of_coffe = str(typeOfCoffe)
+class Blender(Device):
+    def __init__(self, type, model, date, power, size):
+        super().__init__(type, model, date)
+        self.power = int(power)
+        self.size = float(size)
+class MeatGrinder(Device):
+    def __init__(self, type, model, date, typeOfMeat, energy):
+        super().__init__(type, model, date)
+        self.typeOfMeat = str(typeOfMeat)
+        self.energy = int(energy)
